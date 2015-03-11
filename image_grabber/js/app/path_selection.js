63159bfcee440301d29c9c7095a6cc2ee9eb4da7
Jason <Chi>
2015-03-11 02:42:55 -0400

    First Commit

---
+++ image_grabber/js/app/path_selection.js
@@ -0,0 +1,15 @@
+define(function(require, exports, module) {
+    function PathSelection() {
+
+    }
+
+    PathSelection.prototype.deactive = function() {
+
+    };
+
+    PathSelection.prototype.active = function() {
+
+    };
+
+    module.exports = new PathSelection();
+});
